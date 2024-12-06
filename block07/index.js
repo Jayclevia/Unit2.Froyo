7767366eda7ba9e2fc8bc342e8d1ac4488e6d1cd
Jayson Sims <jayclevia11@outlook.com>
2024-12-05 21:11:10 -0600

    da vault assignment or some like dat

---
+++ block07/index.js
@@ -0,0 +1,26 @@
+/*
+
+1. store equation for 10 in a variable using add
+2. store equation for 40 in a variable using multip
+3. store equation for 39 in a variable using subt
+4. store secret code message for user by using literals in a variable
+5. display alert box with final message and code
+
+*/
+
+
+
+//1. store equation for 10 in a variable using add
+const just = 5 + 5;
+
+//2. store equation for 40 in a variable using multip
+const some = 4 * 10;
+
+//3. store equation for 39 in a variable using subt
+const numbers = 60 - 29;
+
+//4. store secret code message for user by using literals in a variable
+const theSentence = `You have received this message because you have been chosen to open an important vault. Here is the secret combination: ${just} ${some} ${numbers}`;
+
+//5. display alert box with final message and code
+alert(theSentence)
